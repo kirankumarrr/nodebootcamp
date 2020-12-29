8a09e32425c2355060c703bb9c4961a33269f7e3
kirankumarrr <kirankumarrr>
2020-12-29 21:49:33 +0530

    feature/seeders:Can read data from files and -i & -d

---
+++ seeder.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+
+//Load env vars
+dotenv.config({
+  path:'./config/config.env'
+})
+
+
+//Load Models
+const Bootcamps = require('./models/Bootcamp')
+
+//Connect to DB
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false,
+  useUnifiedTopology: true
+})
+
+//Read JSON files
+
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'))
+
+
+// Import into DB
+const importData = async()=>{
+  try{
+    await Bootcamps.create(bootcamps)
+    console.log("Data Imported".green.inverse);
+    process.exit()
+  }catch(err){
+    console.log('Error::::=>>>',err);
+  }
+}
+
+//Delete the data
+const deleteData = async()=>{
+  try{
+    await Bootcamps.deleteMany(); //Delete all of them
+    console.log("Data Destroyed".red.inverse);
+    process.exit()
+  }catch(err){
+    console.log('Error::::=>>>',err);
+  }
+}
+
+
+if(process.argv[2]==='-i'){
+  importData()
+}else if(process.argv[2]==='-d'){
+  deleteData()
+}
